ea1d36e161ada5e6ebcf093f45b50349cc85a43f
Mac-ziis <macg286@gmail.com>
2023-08-27 14:58:01 -0700

    Add functioning UI for the rectangle tracker

--- ShapeTracker/program.cs
+++ ShapeTracker/program.cs
@@ -9,6 +9,19 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
+      Console.WriteLine("Enter (tri) for Triangle or (rec) for Rectangle:");
+      string introResponse = Console.ReadLine(); 
+      if (introResponse == "tri")
+      {
+        TriangleTracker();
+      }
+      else if (introResponse == "rec")
+      {
+        RectangleTracker();
+      }
+    }
+      static void TriangleTracker()
+      {
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
       Console.WriteLine("Please enter a number:");
       string stringNumber1 = Console.ReadLine();  
@@ -22,6 +35,7 @@ namespace ShapeTracker
       Triangle tri = new Triangle(length1, length2, length3);
       ConfirmOrEditTriangle(tri);
     }
+
 
     static void ConfirmOrEditTriangle(Triangle tri)
     {
@@ -67,8 +81,69 @@ namespace ShapeTracker
       }
       else
       {
-        Console.WriteLine("Goodbye!");
+        Console.WriteLine("Calculate the sides of your rectangle!!");
+      }
+      RectangleTracker();
+    }
+    static void RectangleTracker()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Shape Tracker app again!");
+      Console.WriteLine("We'll calculate what type of rectangle you have based off of the lengths of the rectangles sides.");
+      Console.WriteLine("Please enter a number:");
+      string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("Enter another number:");
+      string stringNumber2 = Console.ReadLine();  
+      int length1 = int.Parse(stringNumber1);  
+      int length2 = int.Parse(stringNumber2);   
+      Rectangle rec = new Rectangle(length1, length2);
+      ConfirmOrEditRectangle(rec);
+    }
+    static void ConfirmOrEditRectangle(Rectangle rec)
+    {
+      Console.WriteLine("Please confirm that you entered in your rectangle correctly:");
+      Console.WriteLine($"Side 1 has a length of {rec.Side1}.");
+      Console.WriteLine($"Side 2 has a length of {rec.Side2}.");
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the rectangle's sides");
+      string userInput = Console.ReadLine();
+      if (userInput == "yes")
+      {
+        CheckRectangleType(rec);
+      }
+      else
+      {
+        Console.WriteLine("Let's fix your rectangle. Please enter the 2 sides again!");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();  
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine();
+        rec.Side1 = int.Parse(stringNumber1);  
+        rec.Side2 = int.Parse(stringNumber2);  
+        ConfirmOrEditRectangle(rec);
       }
     }
+
+static void CheckRectangleType(Rectangle rec)
+    {
+      string result = rec.CheckType();
+      Console.WriteLine("-----------------------------------------");
+      Console.WriteLine("Your result is: " + result + ".");
+      Console.WriteLine("-----------------------------------------");
+      Console.WriteLine("What's next?");
+      Console.WriteLine("Would you like to check a new rectangle (new)?");
+      Console.WriteLine("Please enter 'new' to check the type of a new rectangle. To exit, enter any key.");
+      string userResponse = Console.ReadLine(); 
+      if (userResponse == "new" || userResponse == "New")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Calculate the sides of your rectangle!!");
+      }
+      
+    }
+
+ 
   }
 }
